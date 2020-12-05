0c4e71846a9bcbc0cdbba0125ef75b9bc4545d75
Pawel Palasz <ZNK@bec.dk>
2020-12-05 21:40:09 +0100

    Add project files.

---
+++ GarbageTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace GarbageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------Hello Garbage Collector Tests!");
+            Test();
+            Console.WriteLine("---------press [Enter] to exit!");
+            Console.ReadLine();
+        }
+
+        static void Test()
+        {
+            
+            var smiec1 = new Smiec();
+            using (var smiec = new Smiec())
+            {
+                Console.WriteLine($" smiec {smiec.GetHashCode()} generacja {GC.GetGeneration(smiec)}");
+
+                Thread.Sleep(2000);
+
+                Console.WriteLine($" smiec {smiec.GetHashCode()} generacja {GC.GetGeneration(smiec)}");
+            }
+            Console.WriteLine($" smiec1 {smiec1.GetHashCode()} generacja {GC.GetGeneration(smiec1)}");
+
+
+            smiec1.Dispose();
+            smiec1 = null;
+
+            Thread.Sleep(2000);
+
+            Console.ReadLine();
+
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.Collect();
+        }
+    }
+}
---
+++ GarbageTest/Smiec.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarbageTest
+{
+    public class Smiec:IDisposable
+    {
+        static Smiec()
+        {
+            Console.WriteLine($"Smiec class created");
+        }
+        
+        public Smiec()
+        {
+            Console.WriteLine($"Smiec {this.GetHashCode()} created");
+            System.Diagnostics.Trace.WriteLine($"Smiec {this.GetHashCode()} created");
+        }
+
+        ~Smiec()
+        {
+            Console.WriteLine("Smiec is destroyed");
+            System.Diagnostics.Trace.WriteLine("Smiec is destroyed");
+            ReleaseUnmanagedResources();
+        }
+
+        private void ReleaseUnmanagedResources()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            //GC.SuppressFinalize(this);
+            Console.WriteLine($"Smiec {this.GetHashCode()} is disposed");
+            System.Diagnostics.Trace.WriteLine($"Smiec {this.GetHashCode()} is disposed");
+            ReleaseUnmanagedResources();
+            //GC.SuppressFinalize(this);
+        }
+    }
+}
